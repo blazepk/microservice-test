ed713a83a5f80ff412c1ec3523f9f0a41f4c837e
blazepk <pratyushkk880@gmail.com>
2023-03-15 23:31:01 +0530

    Adding testing to the suite

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,34 +1,7 @@
-import express from "express"
-import "express-async-errors"
-import { json } from "body-parser"
-import mongoose from "mongoose"
-import cookieSession from "cookie-session"
-//
-import { currentUserRouter } from "./routes/current-user"
-import { signinRouter } from "./routes/signin"
-import { signoutRouter } from "./routes/signout"
-import { signupRouter } from "./routes/signup"
-import { errorHandler } from "./middlewares/error-handler"
-import { NotFoundError } from "./errors/not-found-error"
-import { DatabaseConnectionError } from "./errors/database-connection-error"
 
-const app = express()
-app.set("trust proxy" , true)
-app.use(json())
-app.use(cookieSession({
-    name : "session" ,
-    signed : false,
-    secure : true , 
-    httpOnly : true
-}))
-app.use("/api/users", currentUserRouter)
-app.use("/api/users", signinRouter)
-app.use("/api/users", signoutRouter)
-app.use("/api/users", signupRouter)
-app.all("*" , () => {
-    throw new NotFoundError()
-})
-app.use(errorHandler)
+import mongoose from "mongoose"
+import {app} from "./app"
+import { DatabaseConnectionError } from "./errors/database-connection-error"
 
 const start = async () => {
     if(!process.env.JWT_KEY){
