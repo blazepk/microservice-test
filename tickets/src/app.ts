f154b5b04dc200c54b4558a5e27bd59a07792225
blazepk <pratyushkk880@gmail.com>
2023-03-18 15:54:22 +0530

    ticketing service working fine

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import "express-async-errors"
 import { json } from "body-parser"
-import mongoose from "mongoose"
 import cookieSession from "cookie-session"
 //
-import { errorHandler , NotFoundError } from "@ticket-template/common"
+import { errorHandler , NotFoundError , currentUser } from "@ticket-template/common"
+import { createTicketRouter } from "./routes/new"
+import { indexTicketRouter } from "./routes/index"
+import { showTicketRouter } from "./routes/show"
+import { updateTicketRouter } from "./routes/update"
 
 const app = express()
 app.set("trust proxy" , true)
@@ -15,10 +18,15 @@ app.use(cookieSession({
     secure : process.env.NODE_ENV !== "test" , 
     httpOnly : process.env.NODE_ENV !== "test"
 }))
+app.use(currentUser)
 
+app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(indexTicketRouter)
+app.use(updateTicketRouter)
+app.use(errorHandler)
 app.all("*" , () => {
     throw new NotFoundError()
 })
-app.use(errorHandler)
 
 export {app}
